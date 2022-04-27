c10ae470a4ec5cc710646b7705eeff47a41cda09
Aljazek <aradovan5@gmail.com>
2022-04-27 21:44:37 +0100

    Added style for TDL

--- app.js
+++ app.js
@@ -3,27 +3,30 @@ const bodyParser = require("body-parser")
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static("public"))
 
 app.set('view engine', 'ejs');
 
-var items =  ["Wake Up!"] ;
+let items =  ["Wake Up!"] ;
 
 app.get("/", (req, res) => {
 
-    var date = new Date();
-    var options = {
+    let date = new Date();
+    let options = {
         weekday: "long",
         day: "numeric",
         month: "long"
     }
 
-    var today = date.toLocaleDateString('en-us', options);
+    let today = date.toLocaleDateString('en-us', options);
     res.render('list', {theDate: today, newItems: items})
 })
 
 app.post("/", (req,res) => {
-    var item = req.body.item
-    items.push(item);
+    let item = req.body.item
+    if(item != "") {
+        items.push(item);
+    } 
     // console.log(item);
     res.redirect("/")
 })
